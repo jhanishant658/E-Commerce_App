1cf7c98e7a521d7f9e8be755d83d1368cdbacb62
jhanishant658 <jhanishant658@gmail.com>
2026-02-27 10:07:51 +0530

    Add AppStartController with runApp endpoint for application startup

--- E-Commerce_Backend/src/main/java/com/example/E_Commerce/Controllers/AppStartController.java
+++ E-Commerce_Backend/src/main/java/com/example/E_Commerce/Controllers/AppStartController.java
@@ -1,4 +1,12 @@
 package com.example.E_Commerce.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppStartController {
+    @GetMapping("/runApp")
+    public String runApp(){
+        return "OK" ;
+    }
 }
